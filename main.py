4b89cb71f0c3315d1113f4af796326eccea7a3cb
Djlavoy <davidlavoy@me.com>
2015-09-19 20:27:19 +0000

    update and add more comments

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import psutil
 import time
 from twilio.rest import TwilioRestClient 
 
+
+# Set Cpu Threshold Percent
 cpu_threshold = 2
+
+# Set Ram Usage Threshold Percent
 ram_threshold = 2
+
+# Set Disk Usage Threshold Percent
 disk_threshold = 10
 
-# put your own Twilio credentials here 
+# Put your own Twilio credentials here 
+# This Info can be found under the the account setting
 ACCOUNT_SID = "" 
 AUTH_TOKEN = "" 
 
@@ -16,30 +23,35 @@ sendto = "1234567890"
 # Sender Number From Twilio
 sender = "+11234567890"
 
+# TwilioRestClient Var
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
+# Get CPU Usage Percent
 a = psutil.cpu_percent(interval=1)
+
+# Get Ram Usage Percent
 b = psutil.virtual_memory().percent
+
+# Get Disk Usage Percent
 c = psutil.disk_usage('/')
 
+# Checks If Disk is above the threshold
+if int(c[3]) >= disk_threshold:
+    client.messages.create(
+	to="{}".format(sendto), 
+	from_="{}".format(sender), 
+	body="fingBox Alert: Disk FreeSpace is above acceptable {}% and is Currently at {}%".format(disk_threshold, c[3]),)
 
-while True:
+# Checks If Ram is above the threshold
+if int(b) >= ram_threshold:
+    client.messages.create(
+        to="{}".format(sendto),
+        from_="{}".format(sender),
+        body="fingBox Alert: Memory is above acceptable {}% and is Currently at {}%".format(ram_threshold, b),)
 
-    if int(c[3]) >= disk_threshold:
-        client.messages.create(
-	    to="{}".format(sendto), 
-	    from_="{}".format(sender), 
-	    body="fingBox Alert: Disk FreeSpace is above acceptable {}% and is Currently at {}%".format(disk_threshold, c[3]),)
-
-    if int(b) >= ram_threshold:
-        client.messages.create(
-                to="{}".format(sendto),
-                from_="{}".format(sender),
-                body="fingBox Alert: Memory is above acceptable {}% and is Currently at {}%".format(ram_threshold, b),)
-
-
-    if int(a) >= cpu_threshold:
-        client.messages.create(
-                to="{}".format(sendto),
-                from_="{}".format(sender),
-                body="fingBox Alert: CPU is above acceptable {}% and is Currently at {}%".format(cpu_threshold, a),)
+# Check If Cpu is above the threshold 
+if int(a) >= cpu_threshold:
+    client.messages.create(
+        to="{}".format(sendto),
+        from_="{}".format(sender),
+        body="fingBox Alert: CPU is above acceptable {}% and is Currently at {}%".format(cpu_threshold, a),)
